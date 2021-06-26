6c639104d2d674bde241fb13524a44bbb31f0f62
Rafael Neves <rafa.mneves@hotmail.com>
2021-06-26 20:09:56 -0300

    connect with database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -21,7 +23,28 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://rmneves:dxLuVHQI7uhlbzSu@cluster0.kukjf.mongodb.net/my-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to the database.' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed.' });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
